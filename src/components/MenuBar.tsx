6b64cb32649226936b971b84000de8a0df07a257
Adam Lay <adam93.lay+git@gmail.com>
2026-03-15 16:52:04 +0000

    .

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,13 +1,13 @@
-"use client";
 
-import { usePathname, useRouter } from "next/navigation";
-import Link from "next/link";
+
+import { useRouterState } from "@tanstack/react-router";
 
 interface MenuBarProps {}
 
 export default function MenuBar({}: MenuBarProps) {
-  const router = useRouter();
-  const path = usePathname();
+  const path = useRouterState({
+    select: (state) => state.location.pathname,
+  });
 
   const buttons = [
     {
@@ -27,12 +27,13 @@ export default function MenuBar({}: MenuBarProps) {
       </div>
       <div className="navbar-end flex gap-2 text-title">
         {buttons.map((button) => (
-          <Link
+          <a
             key={button.path}
             href={button.path}
-            className={`btn btn-ghost drawer-button ${path === button.path ? "btn-active btn-accent" : ""}`}>
+            className={`btn btn-ghost drawer-button ${path === button.path ? "btn-active btn-accent" : ""}`}
+          >
             {button.text}
-          </Link>
+          </a>
         ))}
         {/* <Link href="/rules" className="btn btn-ghost drawer-button">
           Rules
---
+++ src/routes/__root.tsx
@@ -0,0 +1,47 @@
+import MenuBar from "@/components/MenuBar";
+import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
+import appCss from "./globals.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: "utf-8" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      { title: "TanStack Start Starter" },
+      {name: "description", content: "Flames of Orion - Mech Combat Unit Builder and Rules Reference."}
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  component: RootLayout,
+})
+
+
+function RootLayout() {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Wallpoet&display=swap"
+          rel="stylesheet"></link>
+          <HeadContent />
+      </head>
+      <body className="antialiased">
+        <MenuBar />
+        <main className="min-h-screen max-w-4xl mx-auto flex flex-col pt-4">
+          <Outlet />
+        </main>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
